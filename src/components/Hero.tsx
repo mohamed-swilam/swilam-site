36f62f4a3383f99b3cf5bd4c4c63a024d85fbaef
mohamed-swilam <midoeljoker30@gmail.com>
2026-02-19 15:28:41 +0200

    a

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -76,7 +76,7 @@ export const Hero = () => {
             View Projects
           </Button>
           <a
-            href="/mohamed.hamad_Backend.pdf"
+            href="https://drive.google.com/file/d/15Wuyn7YrnwOeYC7DPE7bJQJ40l2misE_/view"
             className="inline-flex items-center justify-center px-4 py-2 rounded-md font-medium border border-primary/50  hover:bg-primary/10 hover:border-primary transition-all duration-300"
             target="_blank" rel="noopener" download
           >
